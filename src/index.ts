9ab401f206dc3bfbda5c16b2244db5a43849aa20
Chibuzor Obiora <cobiora@briza.com>
2022-04-04 03:16:43 +0100

    refactor use buildStreak

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { formattedDate, shouldIncrementOrResetStreakCounter, Streak } from './utils'
+import { buildStreak, formattedDate, shouldIncrementOrResetStreakCounter, Streak } from './utils'
 
 const KEY = 'streak'
 
@@ -6,28 +6,24 @@ export function streakCounter (storage: Storage, date: Date): Streak {
   const streakInLocalStorage = storage.getItem(KEY)
   if (streakInLocalStorage) {
     try {
-      const streak = JSON.parse(streakInLocalStorage)
+      const streak: Streak = JSON.parse(streakInLocalStorage)
       const state = shouldIncrementOrResetStreakCounter(date, streak.lastLoginDate)
       const SHOULD_INCREMENT = state === 'increment'
       const SHOULD_RESET = state === 'reset'
 
       if (SHOULD_INCREMENT) {
-        const updatedStreak: Streak = {
+        const updatedStreak: Streak = buildStreak(date, {
           ...streak,
           currentCount: streak.currentCount + 1,
           lastLoginDate: formattedDate(date)
-        }
+        })
         // store in localStorage
         storage.setItem(KEY, JSON.stringify(updatedStreak))
 
         return updatedStreak
       }
       if (SHOULD_RESET) {
-        const updatedStreak: Streak = {
-          currentCount: 1,
-          startDate: formattedDate(date),
-          lastLoginDate: formattedDate(date)
-        }
+        const updatedStreak: Streak = buildStreak(date)
         // store in localStorage
         storage.setItem(KEY, JSON.stringify(updatedStreak))
 
@@ -39,11 +35,7 @@ export function streakCounter (storage: Storage, date: Date): Streak {
       console.error('Failed to parse streak from localStorage')
     }
   }
-  const streak = {
-    currentCount: 1,
-    startDate: formattedDate(date),
-    lastLoginDate: formattedDate(date)
-  }
+  const streak = buildStreak(date)
 
   // store in localStorage
   storage.setItem(KEY, JSON.stringify(streak))
